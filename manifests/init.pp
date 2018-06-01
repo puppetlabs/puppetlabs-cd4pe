bc0617e6a530b91ffba88be280bae4e18c848e5a
Carl Caum <carl@carlcaum.com>
2018-06-01 16:03:36 -0700

    Fix typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class cd4pe (
 ) {
   include docker
 
-  $db_endpoint = "mysql:://${$mysql_host}:${mysql_port}/${mysql_db}"
+  $db_endpoint = "mysql://${$mysql_host}:${mysql_port}/${mysql_db}"
 
   docker::image { 'puppet/continuous-delivery-for-puppet-enterprise':
     image_tag => $version,
@@ -28,7 +28,7 @@ class cd4pe (
     ],
     env     => [
       "DB_ENDPOINT=${db_endpoint}",
-      "DB_USER=${myql_user}",
+      "DB_USER=${mysql_user}",
       "DB_PASS=${mysql_pass}",
       "DUMP_URI=${$dump_uri}",
       "PFI_SECRET_KEY=${pfi_secret_key}",
