c32af4c6511464010d4a47afa8f64fae13c52a70
Carl Caum <carl@carlcaum.com>
2018-06-01 10:48:11 -0700

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+class cd4pe (
+  String  $mysql_host,
+  String  $mysql_db,
+  Integer $mysql_port,
+  String  $mysql_user,
+  String  $mysql_pass
+  String  $dump_uri,
+  String  $pfi_secret_key,
+  String  $version = "latest",
+) {
+  include docker
+
+  $db_endpoint = "mysql:://${$mysql_host}:${mysql_port}/${mysql_db}"
+
+  docker::image { 'puppet/continuous-delivery-for-puppet-enterprise':
+    image_tag => $version,
+  }
+
+  docker::run { 'cd4pe':
+    image   => 'puppet/continuous-delivery-for-puppet-enterprise',
+    volumes => {
+      "/var/lib/mysql",
+    },
+    ports   => [
+      "8080:8080",
+      "8000:8000",
+      "7000:7000",
+    ],
+    env     => [
+      "DB_ENDPOINT=${db_endpoint}",
+      "DB_USER=${myql_user}",
+      "DB_PASS=${mysql_pass}",
+      "DUMP_URI=${$dump_uri}",
+      "PFI_SECRET_KEY=${pfi_secret_key}",
+    ]
+  }
+}
