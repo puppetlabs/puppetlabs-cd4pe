654233c0e67f9235bb692bf29d339fe99133ab16
Adam Bottchen <adam.bottchen@puppet.com>
2019-03-06 16:15:46 -0800

    Adding task to allow updating license
    This commit adds a task that allows a user to update their license
    file.  This allows for the potential automation of license updates
    when about to expire.

---
+++ tasks/update_license.rb
@@ -0,0 +1,38 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'puppet'
+require 'uri'
+
+Puppet.initialize_settings
+$:.unshift(Puppet[:plugindest])
+
+require 'puppet_x/puppetlabs/cd4pe_client'
+
+params = JSON.parse(STDIN.read)
+hostname                 = params['resolvable_hostname'] || Puppet[:certname]
+username                 = params['root_email']
+password                 = params['root_password']
+license                  = params['license']
+
+
+uri = URI.parse(hostname)
+hostname = "http://#{hostname}" if uri.scheme == nil
+
+
+web_ui_endpoint          = params['web_ui_endpoint'] || "#{hostname}:8080"
+
+begin
+  client = PuppetX::Puppetlabs::CD4PEClient.new(web_ui_endpoint, username, password)
+  res = client.save_license(license)
+  if res.code != '200'
+    raise Puppet::Error "Error while saving license: #{res.body}"
+  end
+
+  puts "License updated"
+
+
+  exit 0
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
