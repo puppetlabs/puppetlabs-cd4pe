cc85e0782632d8ab77066d6e77fad35976d69877
Eric Zounes <eric.zounes@puppet.com>
2019-04-22 15:06:47 -0700

    Initial commit for create_user

---
+++ tasks/create_user.rb
@@ -0,0 +1,38 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'puppet'
+require 'uri'
+
+Puppet.initialize_settings
+$LOAD_PATH.unshift(Puppet[:plugindest])
+
+require 'puppet_x/puppetlabs/cd4pe_client'
+
+params = JSON.parse(STDIN.read)
+hostname              = params['resolvable_hostname'] || Puppet[:certname]
+email                 = params['email']
+username              = params['username']
+password              = params['password']
+first_name            = params['first_name']
+last_name             = params['last_name']
+company_name          = params['company_name']
+
+uri = URI.parse(hostname)
+hostname = "http://#{hostname}" if uri.scheme.nil?
+
+web_ui_endpoint = params['web_ui_endpoint'] || "#{hostname}:8080"
+
+begin
+  client = PuppetX::Puppetlabs::CD4PEClient.new(web_ui_endpoint)
+  res = client.create_user(email, username, password, first_name, last_name, company_name)
+  if res.code != '200'
+    raise Puppet::Error "Error while creating user: #{res.body}"
+  end
+
+  puts 'Created user'
+
+  exit 0
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
