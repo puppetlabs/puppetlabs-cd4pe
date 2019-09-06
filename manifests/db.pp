1ed3ea6d694ccab5c480b2b7a8707795f744f555
Adam Bottchen <adam.bottchen@puppet.com>
2019-09-06 10:52:39 -0700

    (CDPE-2170) Only create cd4pe docker network when using mysql db
    Prior to this commit, we created the cd4pe docker network in all cases.
    This meant that customers had to ensure they were not already using the
    class B network this defaults to consuming.  This additional overhead is
    only needed when a managed mysql DB is used, which is not the default
    for new installations and thus unlikely.
    
    With this commit, we now check whether or not mysql is in use during
    init and only create the additional docker network if so.  The logic
    used to determine that mysql was already in place was down in the
    database code, so that needed to be pulled up to the init level so it
    could be leveraged prior to creating the network.

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,37 +1,20 @@
 class cd4pe::db(
   String[1] $data_root_dir,
-  Optional[String[1]] $db_host                    = undef,
-  Optional[String[1]] $db_name                    = undef,
-  Optional[Sensitive[String[1]]] $db_pass         = undef,
-  Optional[Integer] $db_port                      = undef,
-  Optional[String[1]] $db_prefix                  = undef,
-  Optional[String[1]] $db_user                    = undef,
-  Optional[Enum['mysql','postgres']] $db_provider = undef,
-  Boolean $manage_database                        = true,
+  Optional[String[1]] $db_host                              = undef,
+  Optional[String[1]] $db_name                              = undef,
+  Optional[Sensitive[String[1]]] $db_pass                   = undef,
+  Optional[Integer] $db_port                                = undef,
+  Optional[String[1]] $db_prefix                            = undef,
+  Optional[String[1]] $db_user                              = undef,
+  Optional[Enum['mysql','postgres']] $db_provider           = undef,
+  Optional[Enum['mysql','postgres']] $effective_db_provider = undef,
+  Boolean $manage_database                                  = true,
 ){
 
   assert_private()
 
   if $manage_database {
-    if $db_provider == undef {
-      # Check if the customer is using a mysql db from a previous install
-      $cd4pe_docker_facts = fact('docker.network.cd4pe.Containers')
-      if !empty($cd4pe_docker_facts) {
-        $cd4pe_mysql = $cd4pe_docker_facts.filter |$k, $v| { $v['Name'] == 'cd4pe_mysql' }.values
-        if !empty($cd4pe_mysql) {
-          $_db_provider = 'mysql'
-        } else {
-          $_db_provider = 'postgres'
-        }
-      } else {
-        $_db_provider = 'postgres'
-      }
-    } else {
-      $_db_provider = $db_provider
-    }
-
-
-    if $_db_provider == 'mysql' {
+    if $effective_db_provider == 'mysql' {
       class { 'cd4pe::db::mysql':
         db_host   => $db_host,
         db_name   => $db_name,
