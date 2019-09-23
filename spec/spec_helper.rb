b50d8286cc32529e1d2e0b697faa58d2469b69c0
Eric Zounes <eric.zounes@puppet.com>
2019-09-23 13:56:20 -0700

    (CDPE-1948) Add a unit test for the add_deployment_to_stage helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -142,6 +142,7 @@ RSpec.shared_context 'cd4pe login' do
   let(:test_host) { 'http://puppet.test' }
   let(:login_url) { "#{test_host}/login" }
   let(:hw_config_url) { "#{test_host}/root/hw-config" }
+  let(:workspace_url) { "#{test_host}/carlsCoolWorkspace/ajax" }
   let(:res_cookie) do
     'com.puppet.pipelines.pfi.sid='\
     'ARVyw81QfSnwTXd1DI8ml3b93SIYSD85XWU2Ymg-wZ_tYrc8SnLz3mK5z0EQRc2NNWCaJeaZWFByCE3-VD4gSCvLnfUSjUuVO9f6HbRT5lbZHUiIn91fMocUVLHJ831rXQ==;Path=/;HttpOnly'
@@ -172,5 +173,293 @@ RSpec.shared_context 'cd4pe login' do
     stub_request(:post, login_url)
       .with(body: JSON.generate(req_login))
       .to_return(headers: { 'Set-Cookie' => res_cookie }, body: JSON.generate(res_login))
+  end
+end
+
+RSpec.shared_context 'add_deployment_to_stage test params for a module deployment' do
+  let(:test_module_pipeline_name) { 'module_deployments_src' }
+  let(:test_pipeline_id) { '1riocypice7pm0nko4reah1q9v' }
+  let(:test_module_repo_name) { 'puppetlabs-cd4pe_tests' }
+  let(:test_workspace) { 'carlsCoolWorkspace' }
+  let(:test_pe_creds_name) { 'PE-Github' }
+  let(:test_node_group_name) { 'module_deployments_target' }
+  let(:new_stage_name) { 'newTestStage'}
+  let(:add_stage_after) { 'Code Validation stage' }
+
+  let(:initial_pipeline) do
+    [{
+      buildStage: {
+        stageNum: 0,
+      },
+      id: '1riocypice7pm0nko4reah1q9v',
+      name: 'module_deployments_src',
+      projectId: {
+        domain: 'd3',
+        projectName: 'm.puppetlabs-cd4pe_tests',
+      },
+      sources: [
+        {
+          autoBuildTriggers: ['Commit'],
+          branch: 'module_deployment_src',
+          containerName: 'default',
+          skipBuild: true,
+          trigger: 'SOURCE_REPOSITORY',
+        },
+      ],
+      stages: [
+        {
+          destinations: [
+            {
+              id: '705rtzqjy2vx04fjdswfpma8l',
+              stageNum: 1,
+              vmJobTemplateId: 1,
+              vmJobTemplateName: 'module-pdk-validate',
+            },
+          ],
+          stageName: 'Code Validation stage',
+          stageNum: 1,
+          triggerOn: false,
+        },
+        {
+          destinations: [
+            {
+              id: '1oafk1z9kn53u098tth10vzowi',
+              stageNum: 2,
+              peModuleDeploymentTemplate: {
+                baseTaskUrl: 'http://localhost:8080/carlsCoolWorkspace/module-deployments',
+                settings: {
+                  doCodeDeploy: true,
+                  environment: {
+                    nodeGroupBranch: 'production',
+                    nodeGroupId: '5499bcea-06fd-4f46-a3d9-31aeb9b69f6a',
+                    nodeGroupName: 'Production environment',
+                    peCredentialsId: {
+                      domain: 'd3',
+                      name: 'PE-Github',
+                    },
+                  },
+                  moduleId: {
+                    domain: 'd3',
+                    name: 'puppetlabs-cd4pe_tests',
+                  },
+                },
+              },
+            },
+          ],
+          stageNum: 2,
+          stageName: 'Deployment stage',
+          triggerOn: false,
+        },
+      ],
+    }]
+  end
+
+  let(:updated_stages) do
+    [
+      {
+        destinations: [
+          {
+            id: '705rtzqjy2vx04fjdswfpma8l',
+            stageNum: 1,
+            vmJobTemplateId: 1,
+            vmJobTemplateName: 'module-pdk-validate',
+          },
+        ],
+        stageName: 'Code Validation stage',
+        stageNum: 1,
+        triggerOn: false,
+      },
+      {
+        destinations: [
+          {
+            peModuleDeploymentTemplate: {
+              settings: {
+                doCodeDeploy: true,
+                environment: {
+                  nodeGroupBranch: 'module_deployments_target',
+                  nodeGroupId: 'ac38f5d2-36b7-4ae8-a842-13aecaff6d16',
+                  nodeGroupName: 'module_deployments_target',
+                  peCredentialsId: {
+                    domain: 'd3',
+                    name: 'PE-Github',
+                  },
+                },
+                moduleId: {
+                  domain: 'd3',
+                  name: 'puppetlabs-cd4pe_tests',
+                },
+              },
+            },
+          },
+        ],
+        stageName: new_stage_name,
+        triggerOn: true,
+        triggerCondition: 'AllSuccess',
+      },
+      {
+        destinations: [
+          {
+            id: '1oafk1z9kn53u098tth10vzowi',
+            stageNum: 2,
+            peModuleDeploymentTemplate: {
+              baseTaskUrl: 'http://localhost:8080/carlsCoolWorkspace/module-deployments',
+              settings: {
+                doCodeDeploy: true,
+                environment: {
+                  nodeGroupBranch: 'production',
+                  nodeGroupId: '5499bcea-06fd-4f46-a3d9-31aeb9b69f6a',
+                  nodeGroupName: 'Production environment',
+                  peCredentialsId: {
+                    domain: 'd3',
+                    name: 'PE-Github',
+                  },
+                },
+                moduleId: {
+                  domain: 'd3',
+                  name: 'puppetlabs-cd4pe_tests',
+                },
+              },
+            },
+          },
+        ],
+        stageNum: 2,
+        stageName: 'Deployment stage',
+        triggerOn: false,
+      },
+    ]
+  end
+
+  let(:updated_pipeline) do
+    [{
+      buildStage: {
+        stageNum: 0,
+      },
+      id: '1riocypice7pm0nko4reah1q9v',
+      name: 'module_deployments_src',
+      projectId: {
+        domain: 'd3',
+        projectName: 'm.puppetlabs-cd4pe_tests',
+      },
+      sources: [
+        {
+          autoBuildTriggers: ['Commit'],
+          branch: 'module_deployment_src',
+          containerName: 'default',
+          skipBuild: true,
+          trigger: 'SOURCE_REPOSITORY',
+        },
+      ],
+      stages: [
+        {
+          destinations: [
+            {
+              id: '705rtzqjy2vx04fjdswfpma8l',
+              stageNum: 1,
+              vmJobTemplateId: 1,
+              vmJobTemplateName: 'module-pdk-validate',
+            },
+          ],
+          stageName: 'Code Validation stage',
+          stageNum: 1,
+          triggerOn: false,
+        },
+        {
+          destinations: [
+            {
+              peModuleDeploymentTemplate: {
+                baseTaskUrl: 'http://localhost:8080/carlsCoolWorkspace/module-deployments',
+                settings: {
+                  doCodeDeploy: true,
+                  environment: {
+                    nodeGroupBranch: 'production',
+                    nodeGroupId: '5499bcea-06fd-4f46-a3d9-31aeb9b69f6a',
+                    nodeGroupName: 'Production environment',
+                    peCredentialsId: {
+                      domain: 'd3',
+                      name: 'PE-Github',
+                    },
+                  },
+                  moduleId: {
+                    domain: 'd3',
+                    name: 'puppetlabs-cd4pe_tests',
+                  },
+                },
+              },
+            },
+          ],
+          stageName: 'newTestStage',
+          stageNum: 2,
+          triggerOn: true,
+          triggerCondition: 'AllSuccess',
+        },
+        {
+          destinations: [
+            {
+              id: '1oafk1z9kn53u098tth10vzowi',
+              stageNum: 3,
+              peModuleDeploymentTemplate: {
+                baseTaskUrl: 'http://localhost:8080/carlsCoolWorkspace/module-deployments',
+                settings: {
+                  doCodeDeploy: true,
+                  environment: {
+                    nodeGroupBranch: 'production',
+                    nodeGroupId: '5499bcea-06fd-4f46-a3d9-31aeb9b69f6a',
+                    nodeGroupName: 'Production environment',
+                    peCredentialsId: {
+                      domain: 'd3',
+                      name: 'PE-Github',
+                    },
+                  },
+                  moduleId: {
+                    domain: 'd3',
+                    name: 'puppetlabs-cd4pe_tests',
+                  },
+                },
+              },
+            },
+          ],
+          stageNum: 3,
+          stageName: 'Deployment stage',
+          triggerOn: false,
+        },
+      ],
+    }]
+  end
+
+  let(:puppet_enterprise_environments) do
+    [
+      {
+        classes: {},
+        configData: {},
+        description: 'Production nodes',
+        environment: 'production',
+        environmentTrumps: true,
+        id: '5499bcea-06fd-4f46-a3d9-31aeb9b69f6a',
+        name: 'Production environment',
+        parent: '504f45e9-fee6-490f-b38c-cb4001eeeb95',
+      },
+      {
+        classes: {},
+        configData: {},
+        description: 'Development nodes',
+        environment: 'development',
+        environmentTrumps: true,
+        id: 'c5ad4792-12a5-4cd9-9a2a-ea3d07d384b9',
+        name: 'Development environment',
+        parent: '504f45e9-fee6-490f-b38c-cb4001eeeb95',
+      },
+      {
+        classes: { cd4pe_tests: {} },
+        configData: {},
+        description: 'env for tests',
+        environment: 'module_deployments_target',
+        environmentTrumps: true,
+        id: 'ac38f5d2-36b7-4ae8-a842-13aecaff6d16',
+        name: 'module_deployments_target',
+        parent: '504f45e9-fee6-490f-b38c-cb4001eeeb95',
+        rule: ['or', ['=', 'name', 'cdpe-mod-deployment-2.delivery.puppetlabs.net']],
+        variables: {},
+      },
+    ]
   end
 end
