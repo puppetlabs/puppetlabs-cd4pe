5f5b7056906c1ea798bb07476d790114c51c7f9f
Jonathan Morris <jonathan.morris@puppet.com>
2020-03-12 09:49:47 -0700

    (CDPE-2800) Fix as much Rubocop as possible
    This will still fail Rubocop because I'm using an eval.

--- genParams.rb
+++ genParams.rb
@@ -2,68 +2,68 @@
 
 require 'json'
 
-usage = %q(
-Usage: genParams.rb <storageType> <enableSSL> <sslEndpoint>
+usage = "
+Usage: genParams.rb <storageType> <enableSSL> <ssl_endpoint>
   where <storageType> is one of
   'disk', 's3', or 'artifactory'
 
   <enableSSL> is 'enabled' and requires
-  <sslEndpoint> to be set to a valid FQDN
-)
+  <ssl_endpoint> to be set to a valid FQDN
+"
 
 unless ARGV.length == 3
   abort(usage)
 end
 
-objectStorageType = ARGV[0]
-sslEnabled = ARGV[1] == 'enabled'
-sslEndpoint = ARGV[2]
+object_storage_type = ARGV[0]
+ssl_enabled = ARGV[1] == 'enabled'
+ssl_endpoint = ARGV[2]
 
 # keys needed everywhere
-baseParams = {
-  "root_email"             => "noreply@puppet.com",
-  "root_password"          => "puppetlabs",
-  "generate_trial_license" => true
+base_params = {
+  'root_email'             => 'noreply@puppet.com',
+  'root_password'          => 'puppetlabs',
+  'generate_trial_license' => true,
 }
 
 # keys needed for S3 access
-s3Constants = {
-  "storage_provider" => "S3"
+s3_constants = {
+  'storage_provider' => 'S3',
 }
 
-storageS3 = {
-  "storage_bucket"   => ".storage.S3.s3BucketName",
-  "storage_endpoint" => ".storage.S3.s3Endpoint",
-  "s3_access_key"    => ".storage.S3.awsAccessKey",
-  "s3_secret_key"    => ".storage.S3.secretKey"
+storage_s3 = {
+  'storage_bucket'   => '.storage.S3.s3BucketName',
+  'storage_endpoint' => '.storage.S3.s3Endpoint',
+  's3_access_key'    => '.storage.S3.awsAccessKey',
+  's3_secret_key'    => '.storage.S3.secretKey',
 }
 
 # keys needed for Artifactory access
-artifactoryConstants = {
-  "storage_provider" => "ARTIFACTORY"
+artifactory_constants = {
+  'storage_provider' => 'ARTIFACTORY',
 }
 
-storageArtifactory = {
-  "storage_bucket"           => ".storage.Artifactory.artifactoryGenericBinaryRepositoryName",
-  "storage_endpoint"         => ".storage.Artifactory.artifactoryEndpoint",
-  "artifactory_access_token" => ".storage.Artifactory.artifactoryAccessToken"
+storage_artifactory = {
+  'storage_bucket'           => '.storage.Artifactory.artifactoryGenericBinaryRepositoryName',
+  'storage_endpoint'         => '.storage.Artifactory.artifactoryEndpoint',
+  'artifactory_access_token' => '.storage.Artifactory.artifactoryAccessToken',
 }
 
 # keys needed for SSL
-sslConstants = {
-  "ssl_enabled" => sslEnabled,
-  "ssl_endpoint" => sslEndpoint
+ssl_constants = {
+  'ssl_enabled' => ssl_enabled,
+  'ssl_endpoint' => ssl_endpoint,
 }
 
-sslKeys = {
-  "ssl_server_certificate"    => ".ssl.serverCertificate",
-  "ssl_authority_certificate" => ".ssl.authorityCertificate",
-  "ssl_server_private_key"    => ".ssl.serverPrivateKey",
+ssl_keys = {
+  'ssl_server_certificate'    => '.ssl.serverCertificate',
+  'ssl_authority_certificate' => '.ssl.authorityCertificate',
+  'ssl_server_private_key'    => '.ssl.serverPrivateKey',
 }
 
-def extractKeyValues(jsonBlob, theKeys)
-  theKeys.each do |key, value|
-    theKeys[key] = eval("jsonBlob" + value)
+def extract_key_values(_json_blob, the_keys)
+  the_keys.each do |key, value|
+    the_keys[key] = eval('_json_blob' + value) # rubocop:disable Security/Eval
   end
 end
 
@@ -71,32 +71,32 @@ end
 #
 
 # TODO: check exit status from x%(); parameterize secret object being fetched?
-rawJson = JSON.parse( %x( op get item cdpe-workflow-tests-config.json | jq -r '.details.notesPlain' ).delete('`'), object_class: OpenStruct)
+raw_json = JSON.parse(`op get item cdpe-workflow-tests-config.json | jq -r '.details.notesPlain'`.delete('`'), object_class: OpenStruct)
 
 ## storage type
-case objectStorageType
+case object_storage_type
 when 'disk'
-  storageParams = {}
+  storage_params = {}
 when 's3'
-  storageParams = extractKeyValues(rawJson, storageS3)
-  storageParams = [*s3Constants, *storageParams].to_h
+  storage_params = extract_key_values(raw_json, storage_s3)
+  storage_params = [*s3_constants, *storage_params].to_h
 when 'artifactory'
-  storageParams = extractKeyValues(rawJson, storageArtifactory)
-  storageParams = [*artifactoryConstants, *storageParams].to_h
+  storage_params = extract_key_values(raw_json, storage_artifactory)
+  storage_params = [*artifactory_constants, *storage_params].to_h
 else
-  abort("Unrecognized storage type '#{objectStorageType}' specified")
+  abort("Unrecognized storage type '#{object_storage_type}' specified")
 end
 
 ## enable SSL
-if sslEnabled
-  sslParams = extractKeyValues(rawJson, sslKeys)
-  sslParams = [*sslConstants, *sslParams].to_h
+if ssl_enabled
+  ssl_params = extract_key_values(raw_json, ssl_keys)
+  ssl_params = [*ssl_constants, *ssl_params].to_h
 else
-  sslParams = {}
+  ssl_params = {}
 end
 
-boltKeySet = [*baseParams, *storageParams, *sslParams].to_h
+bolt_key_set = [*base_params, *storage_params, *ssl_params].to_h
 
-File.open("params.json", "w") do |f|
-  f.write(JSON.pretty_generate(boltKeySet))
+File.open('params.json', 'w') do |f|
+  f.write(JSON.pretty_generate(bolt_key_set))
 end
