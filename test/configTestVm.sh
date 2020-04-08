46aeebcdd54f600cb27b69e68aeca8c1ad69bb69
Nate Wolfe <nwolfe@puppet.com>
2020-04-08 14:18:36 -0700

    (maint) Allow SSH key to be configurable
    The key needs to be under your ~/.ssh directory as we have to assume a
    directory to mount into the image.

--- test/configTestVm.sh
+++ test/configTestVm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ssh_key=${SSH_KEY:-id_rsa}
+
 test_image=artifactory.delivery.puppetlabs.net/cd4pe-config-test-vm:latest
 
 docker pull $test_image
@@ -7,6 +9,7 @@ docker pull $test_image
 docker run -ti --rm \
        --volume "$PWD"/..:/app \
        --volume "$HOME"/.ssh:/root/.ssh \
+       --env SSH_KEY="$ssh_key" \
        --env CD4PE_IMAGE="$CD4PE_IMAGE" \
        --env CD4PE_VERSION="$CD4PE_VERSION" \
        $test_image \
